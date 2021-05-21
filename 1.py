bc6066698ad8085e048e4039b9b56bb8324b29b1
Pinewood056 <82595110+Pinewood056@users.noreply.github.com>
2021-05-21 15:31:58 +0300

    Update 1.py
    it finally works

--- 1.py
+++ 1.py
@@ -1,37 +1,43 @@
 from math import sqrt
 
-#a = float(input('Enter first side: '))
-#b = float(input('Enter second side: '))
-#c = float(input('Enter third side: '))
 
 class Triangle:
-    def __init__( self, a=6, b=6, c=9):
+
+    def __init__(self, a=float(input('Enter first side: ')), b=float(input('Enter second side: ')),
+                 c=float(input('Enter third side: '))):
         self.a = a
         self.b = b
         self.c = c
         self.p = (a+b+c) * 0.5
 
+    def is_exist(self):
+        if self.a + self.b < self.c or self.a + self.c < self.b or self.b + self.c < self.a:
+            return False
+        else:
+            return True
 
-#use Gerron to get triangle square
-    def square( self ):
-        return sqrt( self.p * (self.p - self.a) * (self.p - self.b) * (self.p - self.c))
-    def perimeter( self ):
+    def square(self):
+        return sqrt(self.p * (self.p - self.a) * (self.p - self.b) * (self.p - self.c))
+
+    def perimeter(self):
         return self.p * 2
+
     def type(self):
         if self.a == self.b == self.c:
             return "Triangle is equilateral"
         elif self.a == self.b or self.a == self.c or self.b == self.c:
             return "Triangle is isosceles"
-        elif self.a^2 == self.b^2 + self.c^2 or self.b^2 == self.a^2 + self.c^2 or c^2 == self.b^2 + self.a^2:
+        elif self.a * self.a == self.b * self.b + self.c * self.c \
+                or self.a * self.a + self.c * self.c == self.b * self.b \
+                or self.c * self.c == self.b * self.b + self.a * self.a:
             return "Triangle is right"
-        else :
+        else:
             return "Triangle is scalene"
 
 
-try1 = Triangle()
-
-
-#It`s the end for Perimeter and square
-print ("Square is", try1.square())
-print ("Perimeter is", try1.perimeter())
-print (try1.type())
+if Triangle().is_exist() is True:
+    print('Square is', Triangle().square())
+    print('Perimeter is', Triangle().perimeter())
+    print(Triangle().type())
+else:
+    print('Triangle doesnt exist')
