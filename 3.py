6b65954cfee25e63f5f203f3bf4fd5c5f6c0f461
Pinewood056 <82595110+Pinewood056@users.noreply.github.com>
2021-06-01 15:47:38 +0300

    Delete 3.py

--- 3.py
+++
@@ -1,7 +0,0 @@
-import random
-# Generate 1000 random numbers between -1000 and 1000
-randomList = random.sample(range(-1000, 1000), 1000)
-# Sort with Reverse
-randomList.sort(reverse=True)
-# List only 2 first value
-print(randomList[:2])
