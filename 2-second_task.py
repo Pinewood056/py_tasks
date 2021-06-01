6cb635940e1eacbdb3f5f422437463822401374a
Pinewood056 <82595110+Pinewood056@users.noreply.github.com>
2021-06-01 19:06:13 +0300

    Update 2-second_task.py
    Some improve

--- 2-second_task.py
+++ 2-second_task.py
@@ -7,8 +7,8 @@ def average():
     return sum(random_list)/len(random_list)
 
 
-average08 = int(average()-200)
-average12 = int(average()+200)
+average08 = average() * 0.8
+average12 = average() * 1.2
 
 print(random_list)
 print("Average is", average())
@@ -17,8 +17,15 @@ print("120% from average is", average12)
 
 finalArray = []
 
-for i in random_list:
-    if average08 < i < average12:
-        finalArray.append(i)
+if average() > 0:
+    for i in random_list:
+        if average08 < i < average12:
+            finalArray.append(i)
+if average() < 0:
+    for i in random_list:
+        if average08 > i > average12:
+            finalArray.append(i)
+if average() == 0:
+    print('You are winner. Average is Zero!')
 
 print(finalArray)
