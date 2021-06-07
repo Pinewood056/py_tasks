b3251ff6f53abfbb32d148059238122b151fcc35
Pinewood056 <82595110+Pinewood056@users.noreply.github.com>
2021-06-07 15:08:22 +0300

    Update 5-fifth.py

--- 5-fifth.py
+++ 5-fifth.py
@@ -1,16 +1,23 @@
-import datetime
+class DateTask:
 
-inputDate = input("To check the date, enter the date in format 'dd,mm,yy' : ")
+    def __init__(self, dd, mm, yyyy):
+        self.dd = dd
+        self.mm = mm
+        self.yyyy = yyyy
 
-day, month, year = inputDate.split(',')
-isValidDate = True
-
-try:
-    datetime.datetime(int(year), int(month), int(day))
-except ValueError:
-    isValidDate = False
-
-if isValidDate:
-    print("This date is exist")
-else:
-    print("This date isn`t exist")
+    def check(self):
+        if self.dd <= 30 and self.mm == 1 or self.dd <= 30 and self.mm == 4 or self.dd <= 30 and self.mm == 6 or\
+                self.dd <= 30 and self.mm == 9 or self.dd <= 30 and self.mm == 11:
+            return True
+        elif self.dd <= 29 and self.mm == 2 and self.yyyy % 4 == 0:
+            return True
+        elif self.dd <= 31 and self.mm == 3 or self.dd <= 31 and self.mm == 5 or self.dd <= 31 and self.mm == 7 or\
+                self.dd <= 31 and self.mm == 8 or self.dd <= 31 and self.mm == 10 or self.dd <= 31 and self.mm == 12:
+            return True
+        elif self.dd <= 28 and self.mm == 2 and self.yyyy % 4 != 0:
+            return True
+        elif self.dd >= 32 or self.mm >= 13:
+            return False
+        else:
+            return False
+        
