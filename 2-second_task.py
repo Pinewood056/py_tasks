c11239fc155989872cb720faebcb80c5bc3db69d
Pinewood056 <82595110+Pinewood056@users.noreply.github.com>
2021-06-07 15:01:36 +0300

    Update 2-second_task.py

--- 2-second_task.py
+++ 2-second_task.py
@@ -1,31 +1,35 @@
 import random
-# Generate 1000 random numbers between -1000 and 1000
 random_list = random.sample(range(-1000, 1000), 1000)
 
 
-def average():
-    return sum(random_list)/len(random_list)
+class Average:
+    def average(self):
+        return sum(random_list)/len(random_list)
+
+    def average08(self):
+        return Average.average(self) * 0.8
+
+    def average12(self):
+        return Average.average(self) * 1.2
 
 
-average08 = average() * 0.8
-average12 = average() * 1.2
+class Final:
+    finalArray = []
+
+    if Average().average() > 0:
+        for i in random_list:
+            if Average().average08() < i < Average().average12():
+                finalArray.append(i)
+    if Average().average() < 0:
+        for i in random_list:
+            if Average().average08() > i > Average().average12():
+                finalArray.append(i)
+    if Average().average() == 0:
+        print('You are winner. Average is Zero!')
+
 
 print(random_list)
-print("Average is", average())
-print("80% from average is", average08)
-print("120% from average is", average12)
-
-finalArray = []
-
-if average() > 0:
-    for i in random_list:
-        if average08 < i < average12:
-            finalArray.append(i)
-if average() < 0:
-    for i in random_list:
-        if average08 > i > average12:
-            finalArray.append(i)
-if average() == 0:
-    print('You are winner. Average is Zero!')
-
-print(finalArray)
+print("Average is", Average().average())
+print("80% from average is", Average().average08())
+print("120% from average is", Average().average12())
+print(Final().finalArray)
