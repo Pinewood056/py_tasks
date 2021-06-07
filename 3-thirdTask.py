ba82af8622af8fdbb55a252faf4527514ca53b5c
Pinewood056 <82595110+Pinewood056@users.noreply.github.com>
2021-06-07 15:05:16 +0300

    Update 3-thirdTask.py

--- 3-thirdTask.py
+++ 3-thirdTask.py
@@ -1,15 +1,19 @@
 import random
-# Generate 1000 random numbers between -1000 and 1000
-randomList = random.sample(range(-1000, 1000), 1000)
-# print the list
-print('The original list:', randomList)
-# print the count of the items of the list
-print('The length of the original list:', len(randomList))
 
-# Sort with Reverse
-randomList.sort(reverse=True)
-print('The sorted list:', randomList)
-# print the count of the items of the sorted list
-print('The length of the sorted list:', len(randomList))
-# List only 2 first value
-print('The first 2 values from the sorted list:', randomList[:2])
+
+class RandomList:
+    randomList = random.sample(range(-1000, 1000), 1000)
+
+    def length_check(self):
+        if len(RandomList.randomList) != 1000:
+            return False
+        else:
+            return True
+
+    randomList.sort(reverse=True)
+    final = randomList[0:2]
+    length = len(final)
+
+
+print('The original list:', RandomList.randomList)
+print(RandomList().final)
