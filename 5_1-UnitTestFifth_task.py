eb253b56b7385c4bff6c9ca21ea66be79f641b7c
Pinewood056 <82595110+Pinewood056@users.noreply.github.com>
2021-06-07 15:09:18 +0300

    Update 5_1-UnitTestFifth_task.py

--- 5_1-UnitTestFifth_task.py
+++ 5_1-UnitTestFifth_task.py
@@ -1,83 +1,22 @@
-import datetime
-# check real date in valid DD, MM, YYYY format
-inputDate = [14, 10, 1994]
-# check yyyy, dd, mm format date
-inputDate2 = [1400, 10, 19]
-# check mm, yyyy, dd format date
-inputDate3 = [12, 1000, 31]
-# check for existing 31 June 2021 (No)
-inputDate4 = [31, 6, 2021]
-# check for existing 31 July 2021 (Yes)
-inputDate5 = [31, 7, 2021]
+import unittest
+from bbb import DateTask
 
 
-def check_date():
-    day, month, year = inputDate
-    is_valid = True
-    try:
-        datetime.datetime(int(year), int(month), int(day))
-    except ValueError:
-        is_valid = False
-    if is_valid:
-        print(inputDate, "- This date is exist")
-    else:
-        print(inputDate, "- This date isn`t exist")
+class TestDate(unittest.TestCase):
+
+    def setUpDateTask(self):
+        self.DateTask = DateTask()
+
+    def test_date_exist(self):
+        self.assertEqual(DateTask(28, 2, 1994).check(), True)
+        self.assertEqual(DateTask(29, 2, 2020).check(), True)
+        self.assertEqual(DateTask(1, 12, 2032).check(), True)
+
+    def test_date_not_exist(self):
+        self.assertEqual(DateTask(32, 12, 2000).check(), False)
+        self.assertEqual(DateTask(11, 13, 2001).check(), False)
+        self.assertEqual(DateTask(29, 2, 2021).check(), False)
 
 
-def check_date2():
-    day, month, year = inputDate2
-    is_valid = True
-    try:
-        datetime.datetime(int(year), int(month), int(day))
-    except ValueError:
-        is_valid = False
-    if is_valid:
-        print(inputDate2, "- This date is exist")
-    else:
-        print(inputDate2, "- This date isn`t exist")
-
-
-def check_date3():
-    day, month, year = inputDate3
-    is_valid = True
-    try:
-        datetime.datetime(int(year), int(month), int(day))
-    except ValueError:
-        is_valid = False
-    if is_valid:
-        print(inputDate3, "- This date is exist")
-    else:
-        print(inputDate3, "- This date isn`t exist")
-
-
-def check_date4():
-    day, month, year = inputDate4
-    is_valid = True
-    try:
-        datetime.datetime(int(year), int(month), int(day))
-    except ValueError:
-        is_valid = False
-    if is_valid:
-        print(inputDate4, "- This date is exist")
-    else:
-        print(inputDate4, "- This date isn`t exist")
-
-
-def check_date5():
-    day, month, year = inputDate5
-    is_valid = True
-    try:
-        datetime.datetime(int(year), int(month), int(day))
-    except ValueError:
-        is_valid = False
-    if is_valid:
-        print(inputDate5, "- This date is exist")
-    else:
-        print(inputDate5, "- This date isn`t exist")
-
-
-check_date()
-check_date2()
-check_date3()
-check_date4()
-check_date5()
+if __name__ == "__main__":
+    unittest.main()
