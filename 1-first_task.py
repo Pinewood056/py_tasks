afa75eb557146950852915581611599bbe846635
Pinewood056 <82595110+Pinewood056@users.noreply.github.com>
2021-06-24 10:28:14 +0300

    Update 1-first_task.py

--- 1-first_task.py
+++ 1-first_task.py
@@ -3,32 +3,35 @@ from math import sqrt
 
 class Triangle:
 
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.p = (a + b + c) * 0.5
+    def __init__(self, side_a, side_b, side_c):
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+        self.semi_perimeter = (side_a + side_b + side_c) * 0.5
 
     def is_exist(self):
-        if self.a + self.b < self.c or self.a + self.c < self.b or self.b + self.c < self.a:
+        if self.side_a + self.side_b < self.side_c or \
+                self.side_a + self.side_c < self.side_b or \
+                self.side_b + self.side_c < self.side_a:
             return False
         else:
             return True
 
     def square(self):
-        return sqrt(self.p * (self.p - self.a) * (self.p - self.b) * (self.p - self.c))
+        return sqrt(self.semi_perimeter * (self.semi_perimeter - self.side_a) * 
+                    (self.semi_perimeter - self.side_b) * (self.semi_perimeter - self.side_c))
 
     def perimeter(self):
-        return self.a + self.b + self.c
+        return self.side_a + self.side_b + self.side_c
 
     def type(self):
-        if self.a == self.b == self.c:
+        if self.side_a == self.side_b == self.side_c:
             return "Triangle is equilateral"
-        elif self.a == self.b or self.a == self.c or self.b == self.c:
+        elif self.side_a == self.side_b or self.side_a == self.side_c or self.side_b == self.side_c:
             return "Triangle is isosceles"
-        elif self.a * self.a == self.b * self.b + self.c * self.c \
-                or self.a * self.a + self.c * self.c == self.b * self.b \
-                or self.c * self.c == self.b * self.b + self.a * self.a:
+        elif self.side_a * self.side_a == self.side_b * self.side_b + self.side_c * self.side_c \
+                or self.side_a * self.side_a + self.side_c * self.side_c == self.side_b * self.side_b \
+                or self.side_c * self.side_c == self.side_b * self.side_b + self.side_a * self.side_a:
             return "Triangle is right"
         else:
             return "Triangle is scalene"
@@ -36,9 +39,9 @@ class Triangle:
 
 # You can set Triangle dimensions here
 # BEFORE USE UNCOMMENT THIS SECTION
-# if Triangle(a=10, b=15, c=17).is_exist() is True:
-#    print('Square is', Triangle(a=10, b=15, c=17).square())
-#    print('Perimeter is', Triangle(a=10, b=15, c=17).perimeter())
-#    print(Triangle(a=10, b=15, c=17).type())
+# if Triangle(side_a=10, side_b=15, side_c=17).is_exist() is True:
+#    print('Square is', Triangle(side_a=10, side_b=15, side_c=17).square())
+#    print('Perimeter is', Triangle(side_a=10, side_b=15, side_c=17).perimeter())
+#    print(Triangle(side_a=10, side_b=15, side_c=17).type())
 # else:
 #    print('Triangle doesnt exist')
